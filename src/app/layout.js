1e6af5f0e34f1ab77dce535a22d4b207e551983e
Dominik Lewczyński <dominiklew200060@gmail.com>
2024-10-17 15:04:57 +0200

    Add import bulma and tag to html data-theme in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import "bulma/css/bulma.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -21,7 +22,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         {children}
