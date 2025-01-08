2be02708ccdec7208f092c905c809d9c6d9f3e43
Dominik Lewczyński <dominiklew200060@gmail.com>
2025-01-08 10:12:08 +0100

    Add responsive scaling to PDFViewer component on window resize

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { pdfjs, Document, Page } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
@@ -7,6 +7,22 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 const PDFViewer = ({ pdfUrl }) => {
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
+    const [pageScale, setPageScale] = useState(1.5);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const screenWidth = window.innerWidth;
+            if (screenWidth < 980) {
+                setPageScale(screenWidth / 980 * 1.3);
+            } else {
+                setPageScale(1.5);
+            }
+        };
+
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
@@ -19,11 +35,11 @@ const PDFViewer = ({ pdfUrl }) => {
                     <>
                         <div className="pdfreact">
                             <div>
-                                <Page className="box" pageNumber={pageNumber} scale={1.5} />
+                                <Page className="box" pageNumber={pageNumber} scale={pageScale} />
                             </div>
                             <div>
                                 {pageNumber + 1 <= numPages && (
-                                    <Page className="box" pageNumber={pageNumber + 1} scale={1.5} />
+                                    <Page className="box" pageNumber={pageNumber + 1} scale={pageScale} />
                                 )}
                             </div>
                         </div>
