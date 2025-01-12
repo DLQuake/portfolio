e7a9aee73c1c0ed36fa7b0c73c76c5b3b72c74f1
Dominik Lewczyński <dominiklew200060@gmail.com>
2025-01-12 17:19:07 +0100

    Move code from ResumePage.js to page.js in resume folder and Delete ResumePage.js

--- src/app/resume/page.js
+++ src/app/resume/page.js
@@ -1,5 +1,22 @@
-import ResumePage from "@/components/ResumePage";
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+import DownloadCVButton from "@/components/DownloadCVButton";
+import { useTranslations } from "next-intl";
+
+const PDFViewer = dynamic(() => import("@/components/PDFViewer"), { ssr: false });
 
 export default function Resume() {
-    return <ResumePage />;
+    const t = useTranslations("ResumePage");
+    const pdfUrl = t("document");
+
+    return (
+        <div className="hero notification p-0 mb-0">
+            <div className="hero-body p-0">
+                <DownloadCVButton pdfUrl={pdfUrl} />
+                <PDFViewer pdfUrl={pdfUrl} />
+                <DownloadCVButton pdfUrl={pdfUrl} />
+            </div>
+        </div>
+    );
 }
