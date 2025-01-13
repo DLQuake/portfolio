76387e58a90f1388253f818d7ee840aaa41f4108
Dominik Lewczyński <dominiklew200060@gmail.com>
2025-01-13 13:56:36 +0100

    Refactor NotFound component to use translations for improved localization

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,16 +1,17 @@
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 export default function NotFound() {
+    const t = useTranslations('NotFound');
+
     return (
         <section className="hero notification is-fullheight">
             <div className="hero-body">
                 <div className="container has-text-centered">
-                    <h1 className="title is-1">404</h1>
-                    <p className="subtitle is-3">Page Not Found</p>
-                    <p className="mb-5">Sorry, the page you are looking for doesn&apos;t exist.</p>
-                    <Link href="/" className="button is-link is-medium">
-                        Back to Home
-                    </Link>
+                    <h1 className="title is-1">{t("title")}</h1>
+                    <p className="subtitle is-3">{t("subtitle")}</p>
+                    <p className="is-size-4 mb-5">{t("content")}</p>
+                    <Link href="/" className="button is-link is-medium">{t("button")}</Link>
                 </div>
             </div>
         </section>
