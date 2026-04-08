0653312eb695f41c21536728657f09446fa86f1f
Dominik Lewczyński <dominiklew200060@gmail.com>
2026-04-08 19:56:32 +0200

    Add new skills and add file .env to .gitignore

--- src/app/about/components/SkillCategories/SkillCategories.js
+++ src/app/about/components/SkillCategories/SkillCategories.js
@@ -1,6 +1,6 @@
-import { FaReact, FaJsSquare, FaNode, FaGitAlt, FaPython, FaHtml5, FaCss3Alt, FaBootstrap, FaSass, FaWordpress } from "react-icons/fa";
+import { FaReact, FaJsSquare, FaGithub, FaChartLine, FaBitbucket, FaNode, FaGitAlt, FaPython, FaHtml5, FaCss3Alt, FaBootstrap, FaSass, FaWordpress } from "react-icons/fa";
 import { RiNextjsFill } from "react-icons/ri";
-import { SiBem, SiBulma, SiExpress, SiFigma, SiFlask, SiJquery, SiMysql, SiNpm, SiPhpmyadmin, SiPostgresql, SiPostman, SiVercel, SiVite } from "react-icons/si";
+import { SiChartdotjs, SiPhp, SiEslint, SiGithubpages, SiPandas, SiNumpy, SiPrestashop, SiMarkdown, SiNodemon, SiP5Dotjs, SiSequelize, SiBem, SiBulma, SiExpress, SiNotion, SiFigma, SiFlask, SiJquery, SiMysql, SiNpm, SiPhpmyadmin, SiPostgresql, SiPostman, SiVercel, SiVite } from "react-icons/si";
 import { VscVscode } from "react-icons/vsc";
 
 export const SkillCategories = [
@@ -16,6 +16,8 @@ export const SkillCategories = [
             { name: "jQuery", icon: <SiJquery /> },
             { name: "React", icon: <FaReact /> },
             { name: "Next.js", icon: <RiNextjsFill /> },
+            { name: "Chart.js", icon: <SiChartdotjs /> },
+            { name: "p5.js", icon: <SiP5Dotjs /> },
         ],
     },
     {
@@ -25,20 +27,8 @@ export const SkillCategories = [
             { name: "Express.js", icon: <SiExpress /> },
             { name: "Python", icon: <FaPython /> },
             { name: "Flask", icon: <SiFlask /> },
-        ],
-    },
-    {
-        category: "Tools",
-        skills: [
-            { name: "Git", icon: <FaGitAlt /> },
-            { name: "Vite", icon: <SiVite /> },
-            { name: "Figma", icon: <SiFigma /> },
-            { name: "VS Code", icon: <VscVscode /> },
-            { name: "Postman", icon: <SiPostman /> },
-            { name: "WordPress", icon: <FaWordpress /> },
-            { name: "Vercel", icon: <SiVercel /> },
-            { name: "NPM", icon: <SiNpm /> },
-            { name: "PhpMyAdmin", icon: <SiPhpmyadmin /> },
+            { name: "Sequelize", icon: <SiSequelize /> },
+            { name: "PHP", icon: <SiPhp /> },
         ],
     },
     {
@@ -46,6 +36,41 @@ export const SkillCategories = [
         skills: [
             { name: "MySQL", icon: <SiMysql /> },
             { name: "PostgreSQL", icon: <SiPostgresql /> },
+        ],
+    },
+    {
+        category: "Tools",
+        skills: [
+            { name: "Git", icon: <FaGitAlt /> },
+            { name: "GitHub", icon: <FaGithub /> },
+            { name: "Bitbucket", icon: <FaBitbucket /> },
+            { name: "Vite", icon: <SiVite /> },
+            { name: "NPM", icon: <SiNpm /> },
+            { name: "Nodemon", icon: <SiNodemon /> },
+            { name: "Figma", icon: <SiFigma /> },
+            { name: "ESlint", icon: <SiEslint /> },
+            { name: "Postman", icon: <SiPostman /> },
+            { name: "VS Code", icon: <VscVscode /> },
+            { name: "Vercel", icon: <SiVercel /> },
+            { name: "GitHub Pages", icon: <SiGithubpages /> },
+            { name: "Notion", icon: <SiNotion /> },
+            { name: "Markdown", icon: <SiMarkdown /> },
+            { name: "PhpMyAdmin", icon: <SiPhpmyadmin /> },
+        ],
+    },
+    {
+        category: "DataScience",
+        skills: [
+            { name: "Numpy", icon: <SiNumpy /> },
+            { name: "Pandas", icon: <SiPandas /> },
+            { name: "Matplotlib", icon: <FaChartLine /> },
+        ],
+    },
+    {
+        category: "CMSEcommerce",
+        skills: [
+            { name: "WordPress", icon: <FaWordpress /> },
+            { name: "PrestaShop", icon: <SiPrestashop /> },
         ],
     },
     {
